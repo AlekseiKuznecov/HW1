7853d67407088cf98696a0e61a09de50521a58bd
AlekseiKuznecov <89993775+AlekseiKuznecov@users.noreply.github.com>
2024-05-27 11:17:01 +0300

    Delete hw1Beans directory

--- hw1Beans/src/main/java/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog implements Pet {
-    @Override
-    public void say() {
-        System.out.println("gav-gav");
-    }
-}
--- hw1Beans/src/main/java/Test1.java
+++
@@ -1,6 +0,0 @@
-public class Test1 {
-    public static void main(String[] args) {
-        Pet pet = new Dog();
-        pet.say();
-    }
-}
